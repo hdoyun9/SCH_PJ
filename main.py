a03a8695f5229eefe8799c26318c04b12ef58a22
hdoyun9 <67012305+hdoyun9@users.noreply.github.com>
2025-01-08 11:17:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ st.write("\n\n")
 
 st.write("Hello! I'm Peter!")
 st.write("My E-mail adress: hdoyun9@gmail.com")
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
